3e1f7b5e286369e9056c8a7ee661ed159701d9a2
thabet720 <thabet.ferjani@esprit.tn>
2023-02-05 17:33:47 +0900

    adding controllers implementation

---
+++ points/src/main/java/com/anymind/points/controller/SalesController.java
@@ -0,0 +1,31 @@
+package com.anymind.points.controller;
+
+import com.anymind.points.dtos.request.SalesAnalyticsRequest;
+import com.anymind.points.dtos.response.SalesAnalyticsResponse;
+import com.anymind.points.services.SalesAnalyticsService;
+import com.anymind.points.validation.util.BeanValidatorUtil;
+import com.anymind.points.validation.util.RuleValidatorUtil;
+
+import java.text.ParseException;
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.stereotype.Controller;
+
+@Controller
+
+public class SalesController {
+	@Autowired
+	SalesAnalyticsService s;
+	@QueryMapping
+	public List<SalesAnalyticsResponse> salesPerHour(@Argument String startDateTime,@Argument String endDateTime) throws ParseException {
+		 SalesAnalyticsRequest request = new SalesAnalyticsRequest(startDateTime, endDateTime);
+		 RuleValidatorUtil.validate(request);
+		 BeanValidatorUtil.validate(request);
+		return s.getSalesAndPointsPerHour( request);
+	}
+}
